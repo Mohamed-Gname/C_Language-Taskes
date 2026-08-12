cb04e577ea16c0e0c421245ded6eddd012dc6ac4
Mohamed Atef Gname <112441942+Mohamed-Gname@users.noreply.github.com>
2026-08-13 01:56:28 +0300

    Implement odd and even sum calculation in C
    The program calculates the sum of even and odd numbers from user input.

---
+++ Assignment_2.c
@@ -0,0 +1,31 @@
+/*Write C code that get array elements from user ,then
+check the elements whether odd or even , and calculate
+the summation of odd and even numbers separately.*/
+#include<stdio.h>
+
+int main()
+{
+	int arr[10];
+	int even_sum = 0;
+	int odd_sum = 0;
+	printf("Enter 10 values: \n");
+	
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+		
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_sum = even_sum + arr[i];
+		}else{
+			odd_sum = odd_sum + arr[i];
+		}
+		
+	}
+	printf("THE EVEN SUM = %d \n", even_sum);
+	printf("THE ODD SUM = %d \n", odd_sum);
+	return 0;
+}
